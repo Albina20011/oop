ab3dd9493e21aa4b51d6db484de44e78fcbe5cbd
Сабит <Aimuratova_A0127@mail.ru>
2020-04-17 23:30:26 +0500

    Assignment 3 OOP

---
+++ src/main/java/kz/aitu/oop/examples/assignment3/Main.java
@@ -0,0 +1,22 @@
+package kz.aitu.oop.examples.assignment3;
+
+public class Main {
+
+    public static void main(String[] args)
+        Scanner input= new Scanner(System.in);
+        Scanner pathSc= new Scanner(System.in);
+
+        int src[]={-2, -1, 0, 1, 2};
+        MyString myObject=new MyString(src);
+        String filename=input.nextLine();
+        String path=pathSc.nextLine();
+
+
+}
+
+
+
+
+
+
+
---
+++ src/main/java/kz/aitu/oop/examples/assignment3/MySpecialString.java
@@ -0,0 +1,42 @@
+package kz.aitu.oop.examples.assignment3;
+
+public class MySpecialString {
+    private int[] values;
+    public MySpecialString(int[] values) {
+        this.values= values;
+    }
+
+    public int length(){
+        return values.length;
+    }
+
+    public int valueAt(int position){
+        if(position>=values.length)
+            return-1;
+        return values[position];
+    }
+
+    public boolean contains(int value){
+        for(int i=0; i< values.length; i++){
+            if(values[i]== value)
+                return true;
+        }
+        return false;
+    }
+
+    public int count(int value) {
+        int count=0;
+        for(int i=0; i<values.length; i++){
+            count++;
+        }
+
+        return count; }
+
+    public int List {
+        for(int i=0; i<values.length; i++){
+            System.out.print (values[i]);
+        }
+    }
+
+}
+}
